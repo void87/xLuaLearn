3c0ba5647b5151308af92258a8540bbd41624006
void87 <void871@163.com>
2021-05-31 14:26:46 +0800

    1. 增加注释

--- xLuaLearn/Assets/Scripts/Test1.cs
+++ xLuaLearn/Assets/Scripts/Test1.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XLua;
+
 
 public class Test1 : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        Debug.Log("Test1.Start()");
+        LuaEnv luaEnv = new LuaEnv();
+        luaEnv.DoString("require 'main'");
     }
 
     // Update is called once per frame
